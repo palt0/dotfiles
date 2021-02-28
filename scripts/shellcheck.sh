ed9db71f2f544a5c47f439e4aa7c364b5c01fca4
Plato <platoo@outlook.it>
2021-02-28 13:23:36 +0100

    Update shellcheck script
    * Now it's not PWD dependant
    * Ignores git submodules

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # shellcheck disable=SC2038
 
+readonly BASE_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
+
 # Filter script files by shebang and `shellcheck` them
-find .. -type f -not -path '*.git*' \
+find "$BASE_DIR" -type f \
+    -not -path "$BASE_DIR/.git*" \
+    -not -path "$BASE_DIR/dotfiles/vim*" \
+    -not -path "$BASE_DIR/dotfiles/shell.d/plugins*" \
     -exec grep -E -ls '^#!(.*/|.*env +)(sh|bash|ksh)' {} \; \
     | xargs shellcheck -e SC1090
